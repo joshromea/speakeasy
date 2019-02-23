f5e1c4c15e27091b45dbfe7661c4b0a590ed2f41
felsuna <felicia.sunanto@gmail.com>
2019-02-23 00:00:32 -0700

    delete button, still not working

---
+++ public/js/history.js
@@ -0,0 +1,19 @@
+/* eslint-disable indent */
+/* eslint-disable prettier/prettier */
+
+$(document).ready(function () {
+    // Delete button for history list.    
+    $(document).on("click", ".delete-history", function () {
+        console.log("delete");
+
+        // API call to delete posts
+        $.ajax({
+            method: "DELETE",
+            url: "/api/Translate" + id
+        })
+            .then(function () {
+
+
+            });
+    });
+});
