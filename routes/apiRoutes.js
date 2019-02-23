dee9ee01308864f82a585957c5ae022bce56a91d
felsuna <felicia.sunanto@gmail.com>
2019-02-23 14:50:29 -0700

    login user check

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,10 +12,13 @@ module.exports = function (app) {
   }
   ))
   app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/',
-    failureRedirect: '/fail'
+    successRedirect: '/'
   }
-  ));
+  ), (req, res, next)=>{
+    if(!req.user){
+      res.json({success: false})
+    }
+  });
   // Get all Users
   app.get("/api/Users", function (req, res) {
     db.User.findAll({}).then(function (dbUsers) {
