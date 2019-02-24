295814cd64f10b6784d8581a0b2c3ecfa0cc34f9
Josh Romea <josh.romea@gmail.com>
2019-02-23 18:35:30 -0700

    Lang API needs looking at

--- routes/LangAPI.js
+++ routes/LangAPI.js
@@ -4,14 +4,8 @@ const transform = require("ee-xml-to-json");
 // Function to change XML String into JSON data//
 function xmlTransform(data) {
     transform(data).then(function (result) {
-<<<<<<< HEAD
         console.log(result);
     });
-=======
-        // SOMEWHERE HERE IT'S NOT RETURNING//
-        return result
-    })
->>>>>>> Need help with LangAPI data
 }
 
 // API pull from Microsoft Lang Translator//
@@ -23,23 +17,14 @@ function langTranslateJSON(startLang, endLang, textString) {
             "X-RapidAPI-Key",
             "wmtOHk6BKgmshNktC1LmQRv1cxBop1RRcDUjsn341ba0oWctPQ"
         )
-        .end(function (result) {
-            // OR SOMEWHERE HERE IT'S NOT RETURNING//
+        .end(result => {
             let xmlString = result.body;
-<<<<<<< HEAD
             xmlTransform(xmlString);
             return xmlTransform;
-=======
-            console.log(xmlString)
-            const newXML = xmlTransform(xmlString)
-            console.log(newXML)
-            return newXML
->>>>>>> Need help with LangAPI data
         });
 }
 
-// BUT WHEN YOU RUN IT THIS LOG WORKS//
-console.log(langTranslateJSON('en', 'es', 'hello+world'))
+// console.log(langTranslateJSON('en', 'es', 'hello+world'))
 
 
 //API pull for Microsoft Lang translate//
